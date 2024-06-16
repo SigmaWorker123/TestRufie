efe9174f7f039ee1bc46fb664e487d5101ac11f0
SigmaWorker123 <eshkeresigmaworker@gmail.com>
2024-06-16 15:54:34 +0300

    ;iubouybgi

--- final_win.py
+++ final_win.py
@@ -1,1 +1,17 @@
 # напиши здесь код третьего экрана приложения
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout
+
+class FinalWin(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.set_appear()
+        self.initUI()
+        self.connects()
+        self.show()
+    def initUI(self):
+        pass
+    def set_appear(self):
+        self.setWindowTitle("RufieTest")
+        self.resize(700,700)
+        self.move(500,700)
