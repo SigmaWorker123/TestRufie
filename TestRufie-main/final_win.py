6457d9eacff447d2e6e551a91de9a7b3a034116a
SigmaWorker123 <eshkeresigmaworker@gmail.com>
2024-06-23 13:06:52 +0300

    Add files via upload

---
+++ TestRufie-main/final_win.py
@@ -0,0 +1,57 @@
+from PyQt5.QtCore import Qt
+
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout
+
+
+
+class FinalWin(QWidget):
+
+    def __init__(self):
+
+        super().__init__()
+
+        self.set_appear()
+
+        self.initUI()
+
+        self.show()
+
+    def initUI(self):
+
+        self.workh_text = QLabel(txt_workheart)
+
+        self.index_text = QLabel(txt_index)
+
+
+
+
+
+        self.layout_line = QVBoxLayout()
+
+        self.layout_line.addWidget(self.index_text, alignment = Qt.AlignCenter)
+
+        self.layout_line.addWidget(self.workh_text, alignment = Qt.AlignCenter)        
+
+        self.setLayout(self.layout_line)
+
+        pass
+
+
+
+    def set_appear(self):
+
+        self.setWindowTitle(txt_finalwin)
+
+        self.resize(win_width, win_height)
+
+        self.move(win_x, win_y)
+
+        pass
+
+
+
+app = QApplication([])
+
+win = FinalWin()
+
+app.exec_()
