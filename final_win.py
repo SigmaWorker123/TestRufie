340e0b4e183d5742216795b878dfcffbf6bfe641
SigmaWorker123 <eshkeresigmaworker@gmail.com>
2024-06-23 14:30:29 +0300

    Update final_win.py

--- final_win.py
+++ final_win.py
@@ -1,17 +1,23 @@
-# напиши здесь код третьего экрана приложения
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout
 
 class FinalWin(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.set_appear()
-        self.initUI()
-        self.connects()
-        self.show()
-    def initUI(self):
-        pass
-    def set_appear(self):
-        self.setWindowTitle("RufieTest")
-        self.resize(700,700)
-        self.move(500,700)
+def __init__(self):
+super().__init__()
+self.set_appear()
+self.initUI()
+self.show()
+def initUI(self):
+self.workh_text = QLabel(txt_workheart)
+self.index_text = QLabel(txt_index)
+
+
+self.layout_line = QVBoxLayout()
+self.layout_line.addWidget(self.index_text, alignment = Qt.AlignCenter)
+self.layout_line.addWidget(self.workh_text, alignment = Qt.AlignCenter)
+self.setLayout(self.layout_line)
+
+def set_appear(self):
+self.setWindowTitle(txt_finalwin)
+self.resize(win_width, win_height)
+self.move(win_x, win_y)
